f256cbf6041dc31c8e4a0781e62e0262875ace8c
JosuSTK <josuecruz214@yahoo.com>
2026-05-16 10:46:00 -0500

    Clase 4_3

--- main.kt
+++ main.kt
@@ -1,12 +1,19 @@
 /// ae_2026_Josue_Cruz_clase_4
 /// estructura del commit clase_4 {{valor incremental}}
 fun main() {
-   val name: String = "Ana"
-   val email:String = "ana@gmail.com"
-   val number: Int = 67
-   // Ana - ana@gmail.com
-   
-   println(name + " - " + email) /// nunca hacer esto
-   println("$name - $email") /// siempre usar string templates en log
-   println("Te faltan ${100 - number} para llegar a 100")
+	val grade: Int = 85 
+    
+    if (grade > 70){
+        println("Aprobado")
+    } else {
+        print("Reprobado")
+    }
+    
+    val result: String = if (grade > 70){
+       "Aprobado"
+    } else {
+        "Reprobado"
+    }
+    
+    println(result)
 }
