9dcfd58bcf601fd7ccc1d8cf800f698c09528965
JosuSTK <josuecruz214@yahoo.com>
2026-05-16 10:27:01 -0500

    Clase 4_1

---
+++ main.kt
@@ -0,0 +1,15 @@
+/// ae_2026_Josue_Cruz_clase_4
+fun main() {
+    /// Siempre debo tipar mis variables (o constantes)
+   val mensaje: String = "Hola mundo"
+   var variablemutable: Int = 5
+   println(mensaje)
+   println(variablemutable)
+     
+   variablemutable = 10
+   println(variablemutable)
+   
+   mensaje = "Hola mundo mutado"
+   println(mensaje)
+
+}
